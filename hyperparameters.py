f7d8d71bfb1b7624d08f7064736824c8fd9bd34e
said-ml <113719075+said-ml@users.noreply.github.com>
2024-02-13 06:11:15 -1100

    reimplementing lr_scheduler

--- hyperparameters.py
+++ hyperparameters.py
@@ -6,5 +6,23 @@ from transformers import TrainerCallback
 
 # implmenting several types of lr_scheduler
 class CustomlLRscheduler(TrainerCallback):
-      def __init__(self):
-          pass
+      '''
+      implementing a linear learning rate decade
+      '''
+      def __init__(self, warmup_step, total_step)->None:
+          '''
+          :param warmup_step:
+          :param total_step:
+          '''
+          assert warmup_step<total_step
+          self.warmup_step=warmup_step
+          self.total_step=total_step
+
+          # define the inintailizer of the parent class
+          super(CustomlLRscheduler, self).__init__()
+
+      def on_epoch_begin(self):
+          '''
+          :return:
+          '''
+
