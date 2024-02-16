2afbc4dcb3218d8487010d987f23eafb251a218e
said-ml <113719075+said-ml@users.noreply.github.com>
2024-02-15 16:17:58 -1100

    pushing file to GitHub

--- sql_file.sql
+++ sql_file.sql
@@ -1,11 +1,11 @@
 /* this is just a test */
--- Create a database named "my_database"
+
 CREATE DATABASE my_database;
 
--- Use the newly created database
+
 USE my_database;
 
--- Create a table named "customers" with four columns
+
 CREATE TABLE customers (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -13,8 +13,7 @@ CREATE TABLE customers (
   phone_number VARCHAR(20)
 );
 
--- Show the created table
+
 SHOW TABLES;
 
--- Describe the table structure
 DESCRIBE customers;
